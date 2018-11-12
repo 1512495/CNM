1f9a7b8dac290e097d58d6c93980f9d39bda0767
tannx <xuantan97@gmail.com>
2018-11-12 13:30:25 +0700

    init socket io

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ var express = require('express'),
     morgan = require('morgan'),
     cors = require('cors');
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var userCtrl = require('./apiController/userCtrl');
 var authRepo = require('./repo/authRepo');
 var verifyAccessToken = require('./repo/authRepo').verifyAccessToken;
@@ -16,18 +19,31 @@ app.use(cors());
 // app.use('/user', verifyAccessToken, userCtrl);
 app.use('/user', userCtrl);
 
-app.get('/login', (req, res)=>{
-	var userEntity = {
+app.get('/login', (req, res) => {
+    var userEntity = {
         Username: 1,
         Password: 'raw pwd'
     }
-	var acToken = authRepo.generateAccessToken(userEntity);
-	res.json({
+    var acToken = authRepo.generateAccessToken(userEntity);
+    res.json({
         acToken
     });
 })
 
+app.get('/', function (req, res) {
+    res.sendStatus(200);
+});
+
+
 var PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`API running on PORT ${PORT}`);
 });
+
+io.on('connection', (socket) => {
+    console.log("A User connected");
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    })
+})
