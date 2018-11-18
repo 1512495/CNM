2ab5d6b8e30c8839a25b5c598b172ec7ac29382b
Phan Văn Tấn <tanphan0805@gmail.com>
2018-11-18 20:46:07 +0700

    update giao dien, validate input app 1

---
+++ frontend/requestReceiver/public/js/index.js
@@ -0,0 +1,52 @@
+$("#bookingBtn").click(function () {
+    $("html, body").animate({ scrollTop: $(document).height() }, 1500);
+});
+
+
+function checkInput() {
+    var x = document.getElementById("phone").value
+    var regex = '[0-9]';
+    if (!x.match(regex))
+    {
+        alert("Must input numbers");
+        return false;
+    }
+}
+
+//Kiểm tra các input của form request (ngoại trừ input note)
+//Nếu có một input rỗng, disable button request
+//ngược lại enable
+function doCheck() {
+    var allFilled = true;
+    $('.request:not(#note)').each(function () {
+        if ($(this).val() == '') {
+            allFilled = false;
+        }
+    });
+    $('#btnDatxe').prop('disabled', !allFilled);
+    if (allFilled) {
+        $('#btnDatxe').removeAttr('disabled');
+    }
+}
+
+//ready event
+$(document).ready(function () {
+    //check
+    doCheck();
+    //event key up request form
+    $('.request').keyup(doCheck);
+    //check login
+    if (Cookies.get('user_auth') == "true") {
+        //thêm tên user
+        $("#userDropdown").append(Cookies.get('user_name'));
+        //hidden login, signup dropdown item
+        //show logout
+        setDropDownItem(true);
+    } else {
+        //show login, signup dropdown item
+        //hidden logout
+        setDropDownItem(false);
+    }
+});
+
+
