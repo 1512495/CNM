9d893ee889f842f97b2124dca0fc78eb81a76f51
Tan <xuantan97@gmail.com>
2018-11-23 14:25:25 +0700

    update realtime driver

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     cors = require('cors');
+userRepo = require('./repo/userRepo');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -45,7 +46,52 @@ http.listen(PORT, () => {
     console.log(`API running on PORT ${PORT}`);
 });
 
+
+var listRequest;
+var currentRequest;
+
+function getPendingRequest() {
+    userRepo.loadAll().then((res) => {
+        listRequest = res;
+    });
+}
+
+
+var rad = function (x) {
+    return x * Math.PI / 180;
+};
+
+var getDistance = function (p1, p2) {
+    if (p1.lat && p2.lat) {
+        var R = 6378137; // Earth’s mean radius in meter
+        var dLat = rad(p2.lat - p1.lat);
+        var dLong = rad(p2.lng - p1.lng);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d; // returns the distance in meter
+    }
+    else {
+        var R = 6378137; // Earth’s mean radius in meter
+        var dLat = rad(p2.lat() - p1.lat());
+        var dLong = rad(p2.lng() - p1.lng());
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d; // returns the distance in meter
+    }
+};
+
+
+var driverList = [];
+var currentDriver;
+
 io.on('connection', (socket) => {
+
     console.log("A User connected");
 
     socket.on("disconnect", () => {
@@ -56,6 +102,12 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('new address added');
     });
     socket.on("driver status", (data) => {
-        console.log(data);
+        if (data.status == 1) {
+            driverList.push(data);
+        }
+        getPendingRequest();
+        if (listRequest) {
+            console.log(listRequest[1]);
+        }
     });
 })
